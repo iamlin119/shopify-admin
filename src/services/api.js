f6c206838254daf39886261cab02c456e3fc9f8f
LIUWENCAN <pheye@qq.com>
2019-12-06 14:22:15 +0800

    直接请求网络数据，支持跨域

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import { query, add } from './rest'
 
-const apiBase = `/admin/api/2019-10`
+const apiBase = `https://mirror.viralbox.org/webtrain/admin/api/2019-10`
 
 export const queryOrders = (params) => query('orders.json', params, apiBase)
 export const addOrder = (params) => add('orders.json', params, apiBase)
